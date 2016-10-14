8751b76e3b5c526ac01a41de1443924bf16d03eb
Kerea <jadelangel87@gmail.com>
2016-10-13 21:11:27 -0500

    first commit. Basic page with search and styled table

---
+++ config/routes.rb
@@ -0,0 +1,14 @@
+Rails.application.routes.draw do
+  devise_for :users
+  get 'welcome/index'
+  get 'welcome/about'
+  get 'welcome/contact'
+  get 'welcome/faq'
+  get 'welcome/pricing'
+  get 'welcome/features'
+
+  resources :invoices
+
+  root to: 'welcome#index'
+
+end
